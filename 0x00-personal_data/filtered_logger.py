c3811ac4368ecce84a22b30268c1e6cced362769
beboo22 <mt557829@gmail.com>
2024-02-08 20:51:23 +0200

    task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ filter_datum
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -14,3 +15,24 @@ def filter_datum(fields: List[str],
         message = re.sub(f'{x}=.*?{separator}',
                          f'{x}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """  Formatter func
+        """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
