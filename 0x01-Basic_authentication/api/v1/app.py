d4fb01844699f3fa8b347a3eaaf89052a0711f8a
beboo22 <mt557829@gmail.com>
2024-02-12 23:14:05 +0200

    task

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,9 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import Auth
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
