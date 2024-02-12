d259104f35bc6e9c314af91c7b654bb243243e03
beboo22 <mt557829@gmail.com>
2024-02-12 22:31:41 +0200

    task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,33 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Return boolean """
+        if path is None:
+            return True
+        
+        if path[-1] != '/':
+            path = path + '/'
+        
+        
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        if path not in excluded_paths:
+            return True
+        
+        if path in excluded_paths:
+            return False
+        
+        x = "/api/v1/status/"
+        if (path == "/api/v1/status" or path == "/api/v1/status/") and x in excluded_paths:
+            return False
+
         return False
 
     def authorization_header(self, request=None) -> str:
         """ Return boolean """
-        return None
+        if request is None or "Authorization" or request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return boolean """
