a2023a58c83c3099f83a158de3c19cc51bca3818
beboo22 <mt557829@gmail.com>
2024-02-13 15:22:16 +0200

    task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""  template for all authentication system
-"""
+"""Template for all authentication system"""
 from api.v1.auth.auth import Auth
 import base64
 
 
 class BasicAuth(Auth):
-    """
-    Manage API authentication methods
-    """
+    """BasicAuth class inherits from Auth class"""
+
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        if authorization_header is None\
-         or not isinstance(authorization_header, str)\
-         or authorization_header[:6] != "Basic ":
+        """Extracts base64 encoded authorization header"""
+        if (authorization_header is None or
+                not isinstance(authorization_header, str) or
+                authorization_header[:6] != "Basic "):
             return None
 
         return authorization_header[6:]
