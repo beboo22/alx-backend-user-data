3d93c3aec6756ed4cfb9470fa45001630b31b418
beboo22 <mt557829@gmail.com>
2024-02-14 19:39:23 +0200

    task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,18 @@ from api.v1.auth.auth import Auth
 from typing import List, TypeVar
 from models.user import User
 import base64
+import uuid
 
 
 class SessionAuth(Auth):
     """BasicAuth class inherits from Auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            None
+        else:
+            SessionID = str(uuid.uuid4())
+            self.user_id_by_session_id[SessionID] = user_id
+            return SessionID
