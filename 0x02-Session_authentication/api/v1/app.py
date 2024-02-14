0cf33680477676a9c8d8a4f7961b866b4c974b6f
beboo22 <mt557829@gmail.com>
2024-02-14 21:34:00 +0200

    task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,10 +52,11 @@ def before_request() -> str:
     """ Not found handler
     """
     list_auth = ['/api/v1/status/', '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/']
+                 '/api/v1/forbidden/','/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, list_auth):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
                 abort(401)
             current_user = auth.current_user(request)
             if current_user is None:
