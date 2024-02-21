9cef5555878b7dde200b7f839aa3389fbda10768
beboo22 <mt557829@gmail.com>
2024-02-21 22:43:40 +0200

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,3 +3,12 @@
 Main file
 """
 import bcrypt
+
+
+def _hash_password(password: str):
+    """Memoized session object
+    """
+    password = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password, salt)
+    return hashed_password
--- 0x03-user_authentication_service/tst.py
+++ 0x03-user_authentication_service/tst.py
@@ -1,3 +1,17 @@
-import sqlalchemy
+# import sqlalchemy
 
-print(sqlalchemy.__version__)
+# print(sqlalchemy.__version__)
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import bcrypt
+
+password = b"beboo"
+salt = bcrypt.gensalt()
+
+hashed_password = bcrypt.hashpw(password, salt)
+
+print(hashed_password)
+# def _hash_password(password:str):
+#     return password.encode("utf-8")
