ba8f8a5fa340a9f2fe000501a3f68a5527b366ae
beboo22 <mt557829@gmail.com>
2024-02-22 01:05:24 +0200

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,16 +2,20 @@
 """
 Main file
 """
-from flask import flask
+from flask import Flask, jsonify
 
 
-app = flask(__name__)
+app = Flask(__name__)
 
 
-@app.route('/', method="GET")
+@app.route('/', methods=["GET"])
 def index() -> str:
     """GET /
     Return:
         - JSON payload containing a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
