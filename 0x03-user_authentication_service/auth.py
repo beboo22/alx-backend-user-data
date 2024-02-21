de69a92d71d99dbea92f3bdf711d3ca8dc3a6d2d
beboo22 <mt557829@gmail.com>
2024-02-22 00:35:25 +0200

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,12 +3,30 @@
 Main file
 """
 import bcrypt
+from user import Base, User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
-    """Memoized session object
-    """
+    """Hash the password using bcrypt"""
     password = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password, salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except BaseException:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
