519a73c679049171a88026d94ec0d56a96d11783
beboo22 <mt557829@gmail.com>
2024-02-22 16:23:16 +0200

    task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,6 +86,9 @@ class Auth:
         return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
+        """Uses reset token to validate update of users password"""
+        if reset_token is None or password is None:
+            return None
         try:
             find_user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
