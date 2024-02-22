cc9c7f185746e2abb06dfd21695b2c5e52b908b5
beboo22 <mt557829@gmail.com>
2024-02-22 16:30:07 +0200

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -94,5 +94,21 @@ def get_reset_password_token() -> Union[str, None]:
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=["PUT"])
+def update_password() -> Union[str, None]:
+    """GET /
+    Return:
+        - JSON payload containing a welcome message.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        reset_token = AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
