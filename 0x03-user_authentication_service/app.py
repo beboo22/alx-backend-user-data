f25c939121f4a284068821e219efee7c94e5fb26
beboo22 <mt557829@gmail.com>
2024-02-22 13:10:22 +0200

    task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,13 @@
 """
 Main file
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from sqlalchemy.exc import InvalidRequestError
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=["GET"])
@@ -14,6 +17,23 @@ def index() -> str:
     Return:
         - JSON payload containing a welcome message.
     """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"])
+def register_users() -> str:
+    """GET /
+    Return:
+        - JSON payload containing a welcome message.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
     return jsonify({"message": "Bienvenue"})
 
 
